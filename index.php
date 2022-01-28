9119a57da68cc1349f4f4ebee43109481fcd0372
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-28 15:17:15 +0100

    Exo 173 ok

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo 173</title>
+</head>
+<body>
+
+<form action="script.php" method="post">
+    <select name="select" id="select">
+        <option value="purple">Violet</option>
+        <option value="red">Rouge</option>
+        <option value="black">Noir</option>
+        <option value="blue">Bleu</option>
+        <option value="pink">Rose</option>
+    </select>
+
+    <input type="submit" value="envoyer">
+</form>
+
+<a href="background2.php">Page2</a> <br>
+<a href="script.php"></a>
+
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>page</title>
+    <?php
+    if ($_POST["select"] === "purple") { ?>
+        <style>
+            body{
+                background-color: #a006b9;
+            }
+        </style>
+        <?php
+        $_SESSION["couleur"] = "purple";
+    }
+    elseif ($_POST["select"] === "red") { ?>
+        <style>
+            body{
+                background-color: #d20808;
+            }
+        </style>
+        <?php
+        $_SESSION["couleur"] = "red";
+    }
+    elseif ($_POST["select"] === "black") { ?>
+        <style>
+            body{
+                background-color: #000000;
+            }
+        </style>
+        <?php
+        $_SESSION["couleur"] = "black";
+    }
+    elseif ($_POST["select"] === "blue") { ?>
+        <style>
+            body{
+                background-color: #1f0ebe;
+            }
+        </style>
+        <?php
+        $_SESSION["couleur"] = "Violet";
+    }
+    elseif ($_POST["select"] === "pink") { ?>
+        <style>
+            body{
+                background-color: #fc00d3;
+            }
+        </style>
+        <?php
+        $_SESSION["couleur"] = "pink";
+    }
+
+    ?>
+</head>
+<body>
+
+<a href="index.php">Index</a>
+<a href="background2.php">Page2</a>
+</body>
+</html>
